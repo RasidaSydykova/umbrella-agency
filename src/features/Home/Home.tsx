909a2124c3265192153c02b8d16d8ba544546d7c
RasidaSydykova <sydykova.rasida@mail.ru>
2024-07-31 21:40:28 +0600

    HomePage: Implemented Main block

---
+++ src/features/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '@/features/Home/Main/Main';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Main />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/features/Home/Main/Main.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import FormLink from '@/components/FormLink/FormLink';
+import './Main.scss';
+
+const Main = () => {
+  const bannerLink = `url("/assets/main-img.jpeg")`;
+  return (
+    <section className="hero-section">
+      <div
+        className="hero-section-banner"
+        style={{ backgroundImage: bannerLink, backgroundSize: 'cover' }}
+      >
+        <div className="hero-section-banner-wrapper container">
+          <h1 className="hero-section-banner-wrapper-title">
+            Мы стремимся стать партнером в digital-маркетинге, которому вы доверяете
+          </h1>
+          <p className="hero-section-banner-wrapper-subtitle">
+            Предлагаем комплексный анализ вашего бизнеса и индивидуальные стратегии для PPC, SEO и
+            других ключевых направлений
+          </p>
+          <div className="hero-section-banner-wrapper-link">
+            <FormLink />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
