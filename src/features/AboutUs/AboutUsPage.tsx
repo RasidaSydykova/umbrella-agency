35e6ea10b90e9fe58f6c29399a05dd0e7593fe00
RasidaSydykova <sydykova.rasida@mail.ru>
2024-08-02 10:14:01 +0600

    AboutUsPage: Implemented OurMission block

--- src/features/AboutUs/AboutUsPage.tsx
+++ src/features/AboutUs/AboutUsPage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Main from '@/features/AboutUs/Main/Main';
 import './AboutUsPage.scss';
 import Founders from '@/features/AboutUs/Founders/Founders';
+import OurMission from '@/features/AboutUs/OurMission/OurMission';
 
 const AboutUsPage = () => {
   return (
     <div className="about-us-page">
       <Main />
       <Founders />
+      <OurMission />
     </div>
   );
 };
---
+++ src/features/AboutUs/OurMission/OurMission.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './OurMission.scss';
+
+const OurMission = () => {
+  return (
+    <section className="our-mission">
+      <div className="our-mission-wrapper container">
+        <h3 className="our-mission-wrapper-title">Наша миссия</h3>
+        <p className="our-mission-wrapper-text">
+          Мы объединяем разносторонний опыт в области digital-маркетинга для создания успешных
+          персонализированных решений, которые помогают клиентам достигать их бизнес-целей с
+          партнером, которому доверяют.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default OurMission;
