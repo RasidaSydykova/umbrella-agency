c94169e85e014b9f16fa33fd85beebfce50b753c
RasidaSydykova <sydykova.rasida@mail.ru>
2024-08-02 16:20:59 +0600

    AboutUs-OurMission block: implemented responsiveness

--- src/features/AboutUs/OurMission/OurMission.tsx
+++ src/features/AboutUs/OurMission/OurMission.tsx
@@ -5,12 +5,21 @@ const OurMission = () => {
   return (
     <section className="our-mission">
       <div className="our-mission-wrapper container">
-        <h3 className="our-mission-wrapper-title">Наша миссия</h3>
-        <p className="our-mission-wrapper-text">
-          Мы объединяем разносторонний опыт в области digital-маркетинга для создания успешных
-          персонализированных решений, которые помогают клиентам достигать их бизнес-целей с
-          партнером, которому доверяют.
-        </p>
+        <div className="our-mission-wrapper-text">
+          <h3 className="our-mission-wrapper-text-title">Наша миссия</h3>
+          <p className="our-mission-wrapper-text-subtext">
+            Мы объединяем разносторонний опыт в области digital-маркетинга для создания успешных
+            персонализированных решений, которые помогают клиентам достигать их бизнес-целей с
+            партнером, которому доверяют.
+          </p>
+        </div>
+        <div className="our-mission-wrapper-img-box">
+          <img
+            className="our-mission-wrapper-img-box-img"
+            src="/assets/about-us/our-mission/our-mission-umbrella.png"
+            alt="Umrella agency"
+          />
+        </div>
       </div>
     </section>
   );
