6c1f7c778e00d28e2b2cf5d2d5d86e7bae856f0c
RasidaSydykova <sydykova.rasida@mail.ru>
2024-08-02 14:56:32 +0600

    AboutUs-Founders block: implemented responsiveness

--- src/features/AboutUs/Founders/Founders.tsx
+++ src/features/AboutUs/Founders/Founders.tsx
@@ -1,12 +1,16 @@
+'use client';
 import React from 'react';
-import './Founders.scss';
 import FounderCard from '@/features/AboutUs/Founders/components/FounderCard/FounderCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import './Founders.scss';
+import 'swiper/css';
 
 const Founders = () => {
   return (
     <section className="founders container">
       <h2 className="founders-title">Основатели</h2>
-      <div className="founders-cards">
+      <div className="founders-cards-desktop">
         {[...Array(3)].map((_, index) => (
           <FounderCard
             key={index}
@@ -16,6 +20,48 @@ const Founders = () => {
             company="Бывший маркетинг-директор компании из списка Forbes"
           />
         ))}
+      </div>
+      <div className="founders-cards-mobile">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={20}
+          centeredSlides={true}
+          pagination={false}
+          navigation={{
+            nextEl: '.mySwiper__btns__next',
+            prevEl: '.mySwiper__btns__prev',
+          }}
+          modules={[Pagination, Navigation]}
+          style={{ overflow: 'visible' }}
+          className="mySwiper"
+          initialSlide={1}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            576: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 1,
+            },
+            993: {
+              slidesPerView: 2,
+            },
+          }}
+        >
+          {[...Array(3)].map((_, index) => (
+            <SwiperSlide>
+              <FounderCard
+                key={index}
+                img="/assets/about-us/founders/founder-img.jpg"
+                text="“Мы верим в силу интегрированного подхода, объединяющего клиентский и агентский опыт”"
+                name="Андрей Александров"
+                company="Бывший маркетинг-директор компании из списка Forbes"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
