d3bf16e1d69530f14a891240540b00ea51a5bf3f
RasidaSydykova <sydykova.rasida@mail.ru>
2024-08-02 10:12:45 +0600

    AboutUsPage: Implemented Main block

---
+++ src/features/AboutUs/AboutUsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '@/features/AboutUs/Main/Main';
+import './AboutUsPage.scss';
+
+const AboutUsPage = () => {
+  return (
+    <div className="about-us-page">
+      <Main />
+    </div>
+  );
+};
+
+export default AboutUsPage;
