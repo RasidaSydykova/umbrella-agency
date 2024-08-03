40d188203088ff3ef49bd390e5669e93abadbaa0
RasidaSydykova <sydykova.rasida@mail.ru>
2024-08-03 11:05:05 +0600

    ServicePage: Created service page

---
+++ src/app/service/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ServicePage from '@/features/Service/ServicePage';
+
+const Service = () => {
+  return <ServicePage />;
+};
+
+export default Service;
---
+++ src/features/Service/ServicePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '@/features/Service/Main/Main';
+
+const ServicePage = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
+
+export default ServicePage;
