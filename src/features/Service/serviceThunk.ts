789f2d4ef512e914945f41455b3a21ed3627b4c9
RasidaSydykova <sydykova.rasida@mail.ru>
2024-08-07 14:23:30 +0600

    Changed responses link

--- src/features/Service/serviceThunk.ts
+++ src/features/Service/serviceThunk.ts
@@ -5,7 +5,7 @@ import { ServicePage } from '../../../types';
 export const fetchServicePage = createAsyncThunk<ServicePage[]>(
   'servicePage/fetchServicePage',
   async () => {
-    const response = await axios.get('http://34.176.229.246/api/v1/service/get/');
+    const response = await axios.get('http://16.170.240.179/api/v1/service/get/');
     return response.data;
   },
 );
